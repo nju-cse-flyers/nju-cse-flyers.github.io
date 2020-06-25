33658ba09b26b9ead593381442a7948cb778dffa
frostwing98 <498924217@qq.com>
2020-06-26 00:33:09 +0800

    update source

---
+++ node_modules/keyword-extractor/lib/stopwords/stopwords.js
@@ -0,0 +1,15 @@
+module.exports = {
+	danish: require("./da").stopwords,
+	dutch: require("./nl").stopwords,
+	english: require("./en").stopwords,
+	french: require("./fr").stopwords,
+	galician: require("./gl").stopwords,
+	german: require("./de").stopwords,
+	italian: require("./it").stopwords,
+	polish: require("./pl").stopwords,
+	portuguese: require("./pt").stopwords,
+	romanian: require("./ro").stopwords,
+	russian: require("./ru").stopwords,
+	spanish: require("./es").stopwords,
+	swedish: require("./se").stopwords
+};
